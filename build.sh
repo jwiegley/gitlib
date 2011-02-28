6d99fa064001db8f41f166e3a4c19ea71d2f98ed
Sakari Jokinen <sakariij@gmail.com>
2011-02-28 17:59:13 +0200

    added build script

---
+++ build.sh
@@ -0,0 +1,27 @@
+set -e
+if [ ! -d libgit2 ]
+then
+    echo "Cloning libgit2 from github"
+    git clone https://github.com/libgit2/libgit2.git libgit2
+fi
+
+echo 
+echo "Building and installing libgit2 library"
+pushd libgit2
+./waf configure
+./waf build
+./waf test
+sudo ./waf install
+popd
+
+echo 
+echo "Generating bindings"
+./create_bindings.rb
+
+echo 
+echo "Starting to build haskell bindings"
+ghc --make setup.hs -o setup
+./setup configure --user --enable-test
+./setup build
+./setup test
+./setup install --user
