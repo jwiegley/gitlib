68b3bdad3b8d0d07389c79429a3ab72b0e30e819
Sakari Jokinen <sakariij@gmail.com>
2011-03-08 23:57:29 +0200

    include libgit2 as a submodule.
    This should make it easier to match the version of libgit2
    to the binding generator.

--- build.sh
+++ build.sh
@@ -1,17 +1,6 @@
 set -e
-if [ ! -d libgit2 ]
-then
-    echo "Cloning libgit2 from github"
-    git clone https://github.com/libgit2/libgit2.git libgit2
-fi
-
-#echo 
-#echo "Building and installing libgit2 library"
-#pushd libgit2
-#python ./waf configure
-#python ./waf build
-#python ./waf test
-#popd
+git submodule init
+git submodule update
 
 echo 
 echo "Generating bindings"
