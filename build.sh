e7b951bae8466a51dac052536165b77569f55988
Sakari Jokinen <sakariij@gmail.com>
2011-08-18 11:13:49 +0300

    compile libgit2 with threads

--- build.sh
+++ build.sh
@@ -6,12 +6,6 @@ cd libgit2
 rm -rf build
 mkdir build
 cd build
-cmake .. 
-cmake --build .
+cmake .. -DTHREADSAFE=ON
+cmake --build . --target install
 popd
-
-echo "Starting to build haskell bindings"
-cabal configure --user --enable-test
-cabal build
-cabal test
-cabal install --user
