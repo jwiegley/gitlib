356ca2bc1b6e384dad198970375496bdf7413b7f
John Wiegley <johnw@fpcomplete.com>
2012-12-17 02:09:59 -0600

    Reading/writing Git objects to S3 works now

--- Data/Git/Backend/S3.hs
+++ Data/Git/Backend/S3.hs
@@ -1,197 +1,236 @@
-module Data.Git.Backend.S3 where
+{-# LANGUAGE ForeignFunctionInterface #-}
 
-import           Aws
-import           Aws.S3
+module Data.Git.Backend.S3 ( odbS3Backend ) where
+
+import           Aws hiding (credentials)
+import           Aws.S3 hiding (bucketName)
+import           Bindings.Libgit2.Odb
 import           Bindings.Libgit2.OdbBackend
 import           Bindings.Libgit2.Oid
 import           Bindings.Libgit2.Types
-import qualified Data.ByteString as B
+import           Control.Applicative
+import           Control.Monad.IO.Class
+import           Data.Attempt
+import           Data.ByteString as B hiding (putStrLn)
+import qualified Data.ByteString.Char8 as BC
 import qualified Data.ByteString.Lazy as BL
--- import           Data.Conduit.Binary
--- import           Data.Conduit.Filesystem
-import           Data.Text (Text)
-import qualified Data.Text as T
--- import           Filesystem
--- import           Filesystem.Path.CurrentOS
+import qualified Data.ByteString.Lazy.Char8 as BLC
+import           Data.ByteString.Unsafe
+import           Data.Conduit
+import           Data.Conduit.Binary
+import           Data.Conduit.List hiding (mapM_, peek)
+import           Data.Git hiding (getObject)
+import           Data.Git.Backend
+import           Data.Git.Error
+import           Data.Git.Oid
+import qualified Data.List as L
+import           Data.Text as T
+import qualified Data.Text.Encoding as E
+import qualified Data.Text.Lazy.Encoding as LE
+import           Debug.Trace (trace)
 import           Foreign.C.String
 import           Foreign.C.Types
 import           Foreign.ForeignPtr
+import           Foreign.Marshal.Alloc
+import           Foreign.Marshal.Utils
 import           Foreign.Ptr
-import           Network.HTTP.Conduit (RequestBody (RequestBodyLBS),
-                                       responseBody, Manager)
+import           Foreign.StablePtr
+import           Foreign.StablePtr
+import           Foreign.Storable
+import           Network.HTTP.Conduit hiding (Response)
+import           Prelude hiding ((.), mapM_)
 
-data GitS3Backend =
-  GitS3Backend { parent      :: ForeignPtr C'git_odb_backend
-               , bucket      :: Text
-               , credentials :: Credentials
-               , manager     :: Manager }
+default (Text)
 
-gitS3_backend__read_header ::
-  Ptr CSize -> Ptr C'git_otype -> Ptr C'git_odb_backend -> Ptr C'git_oid
-  -> IO CInt
-gitS3_backend__read_header len_p type_p _backend oid = undefined
-  -- gitS3_backend *backend;
-  -- int error;
-  -- redisReply *reply;
+data OdbS3Backend = OdbS3Backend { odbS3Parent :: C'git_odb_backend
+                                 , httpManager :: StablePtr Manager
+                                 , bucketName  :: StablePtr Text
+                                 , credentials :: StablePtr Credentials }
 
-  -- assert(len_p && type_p && _backend && oid);
+instance Storable OdbS3Backend where
+  sizeOf p = sizeOf (undefined :: C'git_odb_backend) +
+             sizeOf (undefined :: StablePtr Manager) +
+             sizeOf (undefined :: StablePtr Text) +
+             sizeOf (undefined :: StablePtr Credentials)
+  alignment p = alignment (odbS3Parent p)
+  peek p = do
+    v0 <- peekByteOff p 0
+    let sizev1 = (sizeOf (undefined :: C'git_odb_backend))
+    v1 <- peekByteOff p sizev1
+    let sizev2 = sizev1 + (sizeOf (undefined :: StablePtr Manager))
+    v2 <- peekByteOff p sizev2
+    let sizev3 = sizev2 + (sizeOf (undefined :: StablePtr Text))
+    v3 <- peekByteOff p sizev3
+    return (OdbS3Backend v0 v1 v2 v3)
+  poke p (OdbS3Backend v0 v1 v2 v3) = do
+    pokeByteOff p 0 v0
+    let sizev1 = (sizeOf (undefined :: C'git_odb_backend))
+    pokeByteOff p sizev1 v1
+    let sizev2 = sizev1 + (sizeOf (undefined :: StablePtr Manager))
+    pokeByteOff p sizev2 v2
+    let sizev3 = sizev2 + (sizeOf (undefined :: StablePtr Text))
+    pokeByteOff p sizev3 v3
+    return ()
 
-  -- backend = (gitS3_backend *) _backend;
-  -- error = GIT_ERROR;
+testFileS3 :: OdbS3Backend -> Text -> ResourceT IO [Text]
+testFileS3 odbs3 filepath = do
+  manager <- liftIO $ deRefStablePtr (httpManager odbs3)
+  bucket  <- liftIO $ deRefStablePtr (bucketName odbs3)
+  creds   <- liftIO $ deRefStablePtr (credentials odbs3)
 
-  -- reply = redisCommand(backend->db, "HMGET %b %s %s", oid->id, GIT_OID_RAWSZ,
-  --         "type", "size");
+  let req = (getBucket bucket) {
+                gbMaxKeys = Just 1
+              , gbPrefix  = Just filepath }
+  res <- aws (Configuration Timestamp creds $ defaultLog Error)
+            defServiceConfig manager req
+  gbr <- readResponseIO res
+  return $ L.map objectKey (gbrContents gbr)
 
-  -- if (reply && reply->type == REDIS_REPLY_ARRAY) {
-  --     if (reply->element[0]->type != REDIS_REPLY_NIL &&
-  --             reply->element[0]->type != REDIS_REPLY_NIL) {
-  --         *type_p = (git_otype) atoi(reply->element[0]->str);
-  --         *len_p = (size_t) atoi(reply->element[1]->str);
-  --         error = GIT_SUCCESS;
-  --     } else {
-  --         error = GIT_ENOTFOUND;
-  --     }
-  -- } else {
-  --     error = GIT_ERROR;
-  -- }
+getFileS3 :: OdbS3Backend -> Text
+             -> ResourceT IO (ResumableSource (ResourceT IO) ByteString)
+getFileS3 odbs3 filepath = do
+  manager <- liftIO $ deRefStablePtr (httpManager odbs3)
+  bucket  <- liftIO $ deRefStablePtr (bucketName odbs3)
+  creds   <- liftIO $ deRefStablePtr (credentials odbs3)
 
-  -- freeReplyObject(reply);
-  -- return error;
+  let req = getObject bucket filepath
+  res <- aws (Configuration Timestamp creds $ defaultLog Error)
+            defServiceConfig manager req
 
-gitS3_backend__read ::
-  Ptr (Ptr ()) -> Ptr CSize -> Ptr C'git_otype -> Ptr C'git_odb_backend
-  -> Ptr C'git_oid -> IO CInt
-gitS3_backend__read data_p len_p type_p _backend oid = undefined
-  -- gitS3_backend *backend;
-  -- int error;
-  -- redisReply *reply;
+  gor <- readResponseIO res
+  return (responseBody (gorResponse gor))
 
-  -- assert(data_p && len_p && type_p && _backend && oid);
+putFileS3 :: OdbS3Backend -> Text -> Source (ResourceT IO) ByteString
+              -> ResourceT IO BL.ByteString
+putFileS3 odbs3 filepath src = do
+  manager <- liftIO $ deRefStablePtr (httpManager odbs3)
+  bucket  <- liftIO $ deRefStablePtr (bucketName odbs3)
+  creds   <- liftIO $ deRefStablePtr (credentials odbs3)
 
-  -- backend = (gitS3_backend *) _backend;
-  -- error = GIT_ERROR;
+  lbs <- BL.fromChunks <$> (src $$ consume)
 
-  -- reply = redisCommand(backend->db, "HMGET %b %s %s %s", oid->id,
-  --         GIT_OID_RAWSZ, "type", "size", "data");
+  let req = putObject bucket filepath (RequestBodyLBS lbs)
+  res <- aws (Configuration Timestamp creds $ defaultLog Error)
+            defServiceConfig manager req
 
-  -- if (reply && reply->type == REDIS_REPLY_ARRAY) {
-  --     if (reply->element[0]->type != REDIS_REPLY_NIL &&
-  --             reply->element[1]->type != REDIS_REPLY_NIL &&
-  --             reply->element[2]->type != REDIS_REPLY_NIL) {
-  --         *type_p = (git_otype) atoi(reply->element[0]->str);
-  --         *len_p = (size_t) atoi(reply->element[1]->str);
-  --         *data_p = malloc(*len_p);
-  --         if (*data_p == NULL) {
-  --             error = GIT_ENOMEM;
-  --         } else {
-  --             memcpy(*data_p, reply->element[2]->str, *len_p);
-  --             error = GIT_SUCCESS;
-  --         }
-  --     } else {
-  --         error = GIT_ENOTFOUND;
-  --     }
-  -- } else {
-  --     error = GIT_ERROR;
-  -- }
+  _ <- readResponseIO res
+  return lbs
+
+odbS3BackendReadCallback :: F'git_odb_backend_read_callback
+odbS3BackendReadCallback data_p len_p type_p be oid = do
+  oidStr <- oidToStr oid
+  code   <- odbS3BackendReadHeaderCallback len_p type_p be oid
+  case code of
+    0 -> do
+      odbs3  <- peek (castPtr be :: Ptr OdbS3Backend)
+      result <- runResourceT $ getFileS3 odbs3 (T.pack oidStr)
+      bytes  <- runResourceT $ result $$+- await
+      case bytes of
+        Nothing -> return (-1)
+        Just bs -> do
+          let len = B.length bs
+          content <- mallocBytes (len + 1)
+          unsafeUseAsCString bs $ \cstr ->
+            copyBytes content cstr (len + 1)
+          poke data_p (castPtr content)
+          return 0
+    n -> return n
 
-  -- freeReplyObject(reply);
-  -- return error == GIT_SUCCESS;
+odbS3BackendReadPrefixCallback :: F'git_odb_backend_read_prefix_callback
+odbS3BackendReadPrefixCallback out_oid oid_p len_p type_p be oid len = do
+  oidStr <- oidToStr oid
+  return (-1)
 
--- gitS3_backend_readstream_callback :: Ptr (Ptr <git_odb_stream>) -> Ptr <git_odb_backend> -> Ptr <git_oid> -> IO CInt
+odbS3BackendReadHeaderCallback :: F'git_odb_backend_read_header_callback
+odbS3BackendReadHeaderCallback len_p type_p be oid = do
+  oidStr <- oidToStr oid
+  odbs3  <- peek (castPtr be :: Ptr OdbS3Backend)
+  result <- runResourceT $ getFileS3 odbs3 (T.pack (oidStr ++ ".meta"))
+  bytes  <- runResourceT $ result $$+- await
+  case bytes of
+    Nothing -> return (-1)
+    Just bs -> do let (len,typ) = read (BC.unpack bs) :: (Int,Int)
+                  poke len_p (fromIntegral len)
+                  poke type_p (fromIntegral typ)
+                  return 0
 
-gitS3_backend__read_prefix ::
-  Ptr C'git_oid -> Ptr (Ptr ()) -> Ptr CSize -> Ptr C'git_otype
-  -> Ptr C'git_odb_backend -> Ptr C'git_oid -> CUInt -> IO CInt
-gitS3_backend__read_prefix out_oid data_p len_p type_p _backend short_oid len = undefined
-  -- if (len >= GIT_OID_HEXSZ) {
-  --   /* Just match the full identifier */
-  --   int error = gitS3_backend__read(data_p, len_p, type_p, _backend, short_oid);
-  --   if (error == GIT_SUCCESS)
-  --     git_oid_cpy(out_oid, short_oid);
+odbS3BackendWriteCallback :: F'git_odb_backend_write_callback
+odbS3BackendWriteCallback oid be obj_data len obj_type = do
+  r <- c'git_odb_hash oid obj_data len obj_type
+  case r of
+    0 -> do
+      oidStr <- oidToStr oid
+      odbs3  <- peek (castPtr be :: Ptr OdbS3Backend)
 
-  --   return error;
-  -- } else if (len < GIT_OID_HEXSZ) {
-  --   /* TODO */
-  --   return GIT_ENOTIMPLEMENTED;
-  -- }
+      let metadata = BLC.pack (show ((fromIntegral len,
+                                      fromIntegral obj_type) :: (Int,Int)))
+      runResourceT $ putFileS3 odbs3 (T.pack (oidStr ++ ".meta"))
+                               (sourceLbs metadata)
 
-gitS3_backend__exists :: Ptr C'git_odb_backend -> Ptr C'git_oid -> IO CInt
-gitS3_backend__exists _backend oid = undefined
-  -- gitS3_backend *backend;
-  -- int found;
-  -- redisReply *reply;
+      bytes <- curry unsafePackCStringLen (castPtr obj_data)
+                                         (fromIntegral len)
+      runResourceT $ putFileS3 odbs3 (T.pack oidStr)
+                               (sourceLbs (BL.fromChunks [bytes]))
+      return 0
+    n -> return n
 
-  -- assert(_backend && oid);
+odbS3BackendExistsCallback :: F'git_odb_backend_exists_callback
+odbS3BackendExistsCallback be oid = do
+  oidStr <- oidToStr oid
+  odbs3   <- peek (castPtr be :: Ptr OdbS3Backend)
+  entries <- runResourceT $ testFileS3 odbs3 (T.pack oidStr)
+  if L.length entries == 1
+    then return 0
+    else return (-1)
 
-  -- backend = (gitS3_backend *) _backend;
-  -- found = 0;
+odbS3BackendFreeCallback :: F'git_odb_backend_free_callback
+odbS3BackendFreeCallback be = do
+  backend <- peek be
+  freeHaskellFunPtr (c'git_odb_backend'read backend)
+  freeHaskellFunPtr (c'git_odb_backend'read_prefix backend)
+  freeHaskellFunPtr (c'git_odb_backend'read_header backend)
+  freeHaskellFunPtr (c'git_odb_backend'write backend)
+  freeHaskellFunPtr (c'git_odb_backend'exists backend)
 
-  -- reply = redisCommand(backend->db, "exists %b", oid->id, GIT_OID_RAWSZ);
-  -- if (reply && reply->type != REDIS_REPLY_NIL && reply->type != REDIS_REPLY_ERROR)
-  --     found = 1;
+  odbs3 <- peek (castPtr be :: Ptr OdbS3Backend)
+  freeStablePtr (httpManager odbs3)
+  freeStablePtr (bucketName odbs3)
+  freeStablePtr (credentials odbs3)
 
-  -- freeReplyObject(reply);
-  -- return found;
+foreign export ccall "odbS3BackendFreeCallback"
+  odbS3BackendFreeCallback :: F'git_odb_backend_free_callback
+foreign import ccall "&odbS3BackendFreeCallback"
+  odbS3BackendFreeCallbackPtr :: FunPtr F'git_odb_backend_free_callback
 
-gitS3_backend__write ::
-  Ptr C'git_oid -> Ptr C'git_odb_backend -> Ptr () -> CSize -> C'git_otype
-  -> IO CInt
-gitS3_backend__write oid _backend contents len objType = undefined
-  -- gitS3_backend *backend;
-  -- int error;
-  -- redisReply *reply;
+odbS3Backend :: Manager -> Text -> Text -> Text -> IO (Ptr C'git_odb_backend)
+odbS3Backend manager bucket access secret = do
+  readFun       <- mk'git_odb_backend_read_callback odbS3BackendReadCallback
+  readPrefixFun <-
+    mk'git_odb_backend_read_prefix_callback odbS3BackendReadPrefixCallback
+  readHeaderFun <-
+    mk'git_odb_backend_read_header_callback odbS3BackendReadHeaderCallback
+  writeFun      <- mk'git_odb_backend_write_callback odbS3BackendWriteCallback
+  existsFun     <- mk'git_odb_backend_exists_callback odbS3BackendExistsCallback
 
-  -- assert(id && _backend && data);
+  manager'      <- newStablePtr manager
+  bucket'       <- newStablePtr bucket
+  credentials'  <- newStablePtr (Credentials {
+                                      accessKeyID      = E.encodeUtf8 access
+                                    , secretAccessKey = E.encodeUtf8 secret })
 
-  -- backend = (gitS3_backend *) _backend;
-  -- error = GIT_ERROR;
+  castPtr <$> new OdbS3Backend {
+    odbS3Parent = C'git_odb_backend {
+         c'git_odb_backend'odb         = nullPtr
+       , c'git_odb_backend'read        = readFun
+       , c'git_odb_backend'read_prefix = readPrefixFun
+       , c'git_odb_backend'readstream  = nullFunPtr
+       , c'git_odb_backend'read_header = readHeaderFun
+       , c'git_odb_backend'write       = writeFun
+       , c'git_odb_backend'writestream = nullFunPtr
+       , c'git_odb_backend'exists      = existsFun
+       , c'git_odb_backend'free        = odbS3BackendFreeCallbackPtr }
+    , httpManager = manager'
+    , bucketName  = bucket'
+    , credentials = credentials' }
 
-  -- if ((error = git_odb_hash(id, data, len, type)) < 0)
-  --     return error;
-
-  -- reply = redisCommand(backend->db, "HMSET %b "
-  --         "type %d "
-  --         "size %d "
-  --         "data %b ", id->id, GIT_OID_RAWSZ,
-  --         (int) type, len, data, len);
-
-  -- error = (reply == NULL || reply->type == REDIS_REPLY_ERROR) ? GIT_ERROR : GIT_SUCCESS;
-
-  -- freeReplyObject(reply);
-  -- return error;
-
--- gitS3_backend_writestream_callback :: Ptr (Ptr <git_odb_stream>) -> Ptr <git_odb_backend> -> CSize -> <git_otype> -> IO CInt
-
-gitS3_backend__free :: Ptr C'git_odb_backend -> IO ()
-gitS3_backend__free _backend = undefined
-  -- gitS3_backend *backend;
-  -- assert(_backend);
-  -- backend = (gitS3_backend *) _backend;
-
-  -- redisFree(backend->db);
-
-  -- free(backend);
-
-git_odb_backend_s3 :: String -> String -> String -> GitS3Backend
-git_odb_backend_s3 access secret bucket = undefined
-  -- Ptr (Ptr C'git_odb_backend>)
-  -- backend = calloc(1, sizeof (gitS3_backend));
-  -- if (backend == NULL)
-  --     return GIT_ENOMEM;
-
-  -- backend->db = redisConnect(host, port);
-  -- if (backend->db->err) {
-  --         free(backend);
-  --         return GIT_ERROR;
-  --       }
-
-  -- backend->parent.read = &gitS3_backend__read;
-  -- backend->parent.read_prefix = &gitS3_backend__read_prefix;
-  -- backend->parent.read_header = &gitS3_backend__read_header;
-  -- backend->parent.write = &gitS3_backend__write;
-  -- backend->parent.exists = &gitS3_backend__exists;
-  -- backend->parent.free = &gitS3_backend__free;
-
-  -- *backend_out = (git_odb_backend *) backend;
-
-  -- return 0
+-- S3.hs
---
+++ test/Smoke.hs
@@ -0,0 +1,150 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
+{-# OPTIONS_GHC -fno-warn-wrong-do-bind #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+
+module Main where
+
+import           Control.Applicative
+import           Control.Concurrent.ParallelIO
+import           Control.Monad
+import           Data.Git
+import           Data.Git.Backend
+import           Data.Git.Backend.S3
+import           Data.Git.Backend.Trace
+import           Data.Maybe
+import           Data.Text as T hiding (map)
+import qualified Data.Text.Encoding as E
+import           Data.Time.Clock.POSIX
+import           Data.Traversable
+import           Filesystem (removeTree, isDirectory)
+import           Filesystem.Path.CurrentOS
+import           Network.HTTP.Conduit
+import qualified Prelude
+import           Prelude (putStrLn)
+import           Prelude hiding (FilePath, putStr, putStrLn)
+import           System.Exit
+import           Test.HUnit
+
+import Bindings.Libgit2.OdbBackend
+import Foreign.Marshal.Alloc
+import Foreign.C.String
+import Foreign.Storable
+
+default (Text)
+
+main :: IO ()
+main = do
+  counts' <- runTestTT tests
+  case counts' of
+    Counts _ _ errors' failures' ->
+      if errors' > 0 || failures' > 0
+      then exitFailure
+      else exitSuccess
+  stopGlobalPool
+
+catBlob :: Repository -> Text -> IO (Maybe Text)
+catBlob repo sha = do
+  hash <- stringToOid sha
+  for hash $ \hash' -> do
+    obj <- lookupObject hash' repo
+    case obj of
+      Just (BlobObj b) -> do
+        (_, contents) <- getBlobContents b
+        str <- blobSourceToString contents
+        case str of
+          Nothing   -> return T.empty
+          Just str' -> return (E.decodeUtf8 str')
+
+      Just _  -> error "Found something else..."
+      Nothing -> error "Didn't find anything :("
+
+withRepository :: Text -> (Repository -> Assertion) -> Assertion
+withRepository n f = do
+  let p = fromText n
+  exists <- isDirectory p
+  when exists $ removeTree p
+
+  -- we want exceptions to leave the repo behind
+  f =<< createRepository p True
+
+  removeTree p
+
+oid :: Updatable a => a -> IO Text
+oid = objectId >=> return . oidToText
+
+oidToText :: Oid -> Text
+oidToText = T.pack . show
+
+sampleCommit :: Repository -> Tree -> Signature -> Commit
+sampleCommit repo tr sig =
+    (createCommit repo) { commitTree      = ObjRef tr
+                        , commitAuthor    = sig
+                        , commitCommitter = sig
+                        , commitLog       = "Sample log message." }
+
+tests :: Test
+tests = test [
+
+  "createTwoCommits" ~:
+
+  withRepository "createTwoCommits.git" $ \repo -> do
+    let bucket = "BUCKET"
+        access = "ACCESS"
+        secret = "SECRET"
+
+    -- Store Git objects in S3
+    manager <- newManager def
+    odbs3   <- odbS3Backend manager bucket access secret
+    -- Use the tracing backend to show how much activity is taking place
+    backend <- traceBackend odbs3
+    -- Set the priority to 100 so it overrides the two default backends
+    odbBackendAdd repo backend 100
+
+    let hello = createBlob (E.encodeUtf8 "Hello, world!\n") repo
+    tr <- updateTree "hello/world.txt" (blobRef hello) (createTree repo)
+
+    let goodbye = createBlob (E.encodeUtf8 "Goodbye, world!\n") repo
+    tr <- updateTree "goodbye/files/world.txt" (blobRef goodbye) tr
+    x  <- oid tr
+    (@?=) x "98c3f387f63c08e1ea1019121d623366ff04de7a"
+
+    -- The Oid has been cleared in tr, so this tests that it gets written as
+    -- needed.
+    let sig = Signature {
+            signatureName  = "John Wiegley"
+          , signatureEmail = "johnw@newartisans.com"
+          , signatureWhen  = posixSecondsToUTCTime 1348980883 }
+        c   = sampleCommit repo tr sig
+    x <- oid c
+    (@?=) x "44381a5e564d19893d783a5d5c59f9c745155b56"
+
+    let goodbye2 = createBlob (E.encodeUtf8 "Goodbye, world again!\n") repo
+    tr <- updateTree "goodbye/files/world.txt" (blobRef goodbye2) tr
+    x  <- oid tr
+    (@?=) x "f2b42168651a45a4b7ce98464f09c7ec7c06d706"
+
+    let sig = Signature {
+            signatureName  = "John Wiegley"
+          , signatureEmail = "johnw@newartisans.com"
+          , signatureWhen  = posixSecondsToUTCTime 1348981883 }
+        c2  = (sampleCommit repo tr sig) {
+                  commitLog       = "Second sample log message."
+                , commitParents   = [ObjRef c] }
+    x <- oid c2
+    (@?=) x "2506e7fcc2dbfe4c083e2bd741871e2e14126603"
+
+    -- cid <- objectId c2
+    -- writeRef $ createRef "refs/heads/master" (RefTargetId cid) repo
+    -- writeRef $ createRef "HEAD" (RefTargetSymbolic "refs/heads/master") repo
+
+    -- x <- oidToText <$> lookupId "refs/heads/master" repo
+    -- (@?=) x "2506e7fcc2dbfe4c083e2bd741871e2e14126603"
+
+    -- mapAllRefs repo (\name -> Prelude.putStrLn $ "Ref: " ++ unpack name)
+
+    return()
+
+  ]
+
+-- Main.hs ends here
