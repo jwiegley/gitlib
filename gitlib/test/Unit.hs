451e1db73e8a5bca3c7dfba1331aab26fa092503
John Wiegley <johnw@fpcomplete.com>
2012-12-18 05:42:02 -0600

    Completed move to fpco

--- gitlib/test/Unit.hs
+++
@@ -1,9 +0,0 @@
-{-# OPTIONS_GHC -F -pgmF htfpp #-}
-
-import System.Environment ( getArgs )
-import System.Exit ( exitWith )
-import Test.Framework
-
-test_equality = assertEqual "foo" "foo"
-
-main = htfMain htf_thisModulesTests
