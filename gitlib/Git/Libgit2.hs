69ae171708a6dd39862719ca870879e3a7e9bd31
John Wiegley <johnw@fpcomplete.com>
2013-01-28 01:55:56 -0600

    Get Libgit2.hs to work

--- gitlib/Git/Libgit2.hs
+++ gitlib/Git/Libgit2.hs
@@ -39,6 +39,8 @@ import           Foreign.Ptr
 import           Foreign.Storable
 import qualified Git as Git
 import           Git.Libgit2.Blob
+import           Git.Libgit2.Types
+import           Git.Libgit2.Internal
 import           Prelude hiding (FilePath)
 import           System.IO.Unsafe
 
@@ -64,5 +66,42 @@ instance Git.Repository LgRepository where
     -- createCommit :: [ObjRef Commit] -> ObjRef Tree -> Signature -> Signature
     --                 -> Text -> m c
     createCommit = undefined
+
+-- | Create a new blob in the 'Repository', with 'ByteString' as its contents.
+--
+--   Note that since empty blobs cannot exist in Git, no means is provided for
+--   creating one; if the give string is 'empty', it is an error.
+lgCreateBlob ::
+    Git.BlobContents LgRepository -> LgRepository (Git.BlobOid LgRepository)
+lgCreateBlob b = do
+    repo <- lgGet
+    ptr  <- liftIO $ mallocForeignPtr
+    r    <- Git.blobContentsToByteString b
+            >>= \bs -> liftIO $ createBlobFromByteString repo ptr bs
+    when (r < 0) $ failure Git.BlobCreateFailed
+    return (Tagged (coidToOid ptr))
+
+  where
+    createBlobFromByteString repo coid bs =
+        unsafeUseAsCStringLen bs $
+            uncurry (\cstr len ->
+                      withForeignPtr coid $ \coid' ->
+                      withForeignPtr (repoObj repo) $ \repoPtr ->
+                        c'git_blob_create_frombuffer
+                          coid' repoPtr (castPtr cstr) (fromIntegral len))
+
+lgLookupBlob :: Git.Oid -> LgRepository (Git.Blob m)
+lgLookupBlob oid =
+    lookupObject' oid c'git_blob_lookup c'git_blob_lookup_prefix
+    $ \coid obj _ ->
+    withForeignPtr obj $ \ptr -> do
+        size <- c'git_blob_rawsize (castPtr ptr)
+        buf  <- c'git_blob_rawcontent (castPtr ptr)
+        -- The lifetime of buf is tied to the lifetime of the blob object in
+        -- libgit2, which this Blob object controls, so we can use
+        -- unsafePackCStringLen to refer to its bytes.
+        bstr <- curry unsafePackCStringLen (castPtr buf)
+                      (fromIntegral size)
+        return (Git.Blob (Git.BlobString bstr))
 
 -- Libgit2.hs
--- gitlib/Git/Libgit2/Blob.hs
+++ gitlib/Git/Libgit2/Blob.hs
@@ -4,10 +4,7 @@
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE PatternGuards #-}
 
-module Git.Libgit2.Blob
-       ( lgCreateBlob
-       , lgLookupBlob )
-       where
+module Git.Libgit2.Blob where
 
 import           Bindings.Libgit2
 import           Control.Failure
@@ -20,42 +17,5 @@ import           Foreign.Ptr
 import qualified Git
 import           Git.Libgit2.Internal
 import           Git.Libgit2.Types
-
--- | Create a new blob in the 'Repository', with 'ByteString' as its contents.
---
---   Note that since empty blobs cannot exist in Git, no means is provided for
---   creating one; if the give string is 'empty', it is an error.
-lgCreateBlob ::
-    Git.BlobContents LgRepository -> LgRepository (Git.BlobOid LgRepository)
-lgCreateBlob b = do
-    repo <- lgGet
-    ptr  <- liftIO $ mallocForeignPtr
-    r    <- Git.blobContentsToByteString b
-            >>= \bs -> liftIO $ createBlobFromByteString repo ptr bs
-    when (r < 0) $ failure Git.BlobCreateFailed
-    return (Tagged (coidToOid ptr))
-
-  where
-    createBlobFromByteString repo coid bs =
-        unsafeUseAsCStringLen bs $
-            uncurry (\cstr len ->
-                      withForeignPtr coid $ \coid' ->
-                      withForeignPtr (repoObj repo) $ \repoPtr ->
-                        c'git_blob_create_frombuffer
-                          coid' repoPtr (castPtr cstr) (fromIntegral len))
-
-lgLookupBlob :: Git.Oid -> LgRepository (Git.Blob m)
-lgLookupBlob oid =
-    lookupObject' oid c'git_blob_lookup c'git_blob_lookup_prefix
-    $ \coid obj _ ->
-    withForeignPtr obj $ \ptr -> do
-        size <- c'git_blob_rawsize (castPtr ptr)
-        buf  <- c'git_blob_rawcontent (castPtr ptr)
-        -- The lifetime of buf is tied to the lifetime of the blob object in
-        -- libgit2, which this Blob object controls, so we can use
-        -- unsafePackCStringLen to refer to its bytes.
-        bstr <- curry unsafePackCStringLen (castPtr buf)
-                      (fromIntegral size)
-        return (Git.Blob (Git.BlobString bstr))
 
 -- Blob.hs
