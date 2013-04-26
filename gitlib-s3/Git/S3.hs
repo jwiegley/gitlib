03e09f0d9ccbdf57df6aba74fd04754e710c08d4
John Wiegley <johnw@fpcomplete.com>
2013-04-25 23:05:32 -0500

    Comment out the body of registerObject again

--- gitlib-s3/Git/S3.hs
+++ gitlib-s3/Git/S3.hs
@@ -175,9 +175,10 @@ wrapRegisterObject :: (Text -> Maybe (ObjectLength, ObjectType) -> IO ())
                    -> Maybe (ObjectLength, ObjectType)
                    -> IO ()
 wrapRegisterObject f name metadata =
-    wrap ("Calling registerObject " ++ show name ++ " " ++ show metadata)
-        (f name metadata)
-        (return ())
+    return ()
+    -- wrap ("Calling registerObject " ++ show name ++ " " ++ show metadata)
+    --     (f name metadata)
+    --     (return ())
 
 wrapRegisterPackFile :: (Text -> [Text] -> IO ()) -> Text -> [Text] -> IO ()
 wrapRegisterPackFile f name shas =
