dc32e571c3e0a80a4aabe99255122ac09e3454fc
John Wiegley <johnw@fpcomplete.com>
2013-04-28 16:58:36 -0500

    git-gpush is now working

--- git-gpush/Main.hs
+++ git-gpush/Main.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
-
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ViewPatterns #-}
 
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
@@ -15,15 +15,16 @@ import           Control.Monad
 import           Control.Monad.IO.Class (MonadIO(..))
 import           Control.Monad.Trans.Cont
 import qualified Data.ByteString as B (readFile)
-import           Data.Foldable
+import           Data.Foldable hiding (concat)
 import           Data.Function (fix)
+import           Data.List
 import           Data.Map (Map)
 import qualified Data.Map as Map
 import           Data.Maybe
 import           Data.Tagged
-import qualified Data.Text as T (pack, unpack)
+import qualified Data.Text as T
 import           Data.Text.Lazy (Text)
-import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy as TL hiding (tails)
 import           Data.Time
 import           Data.Traversable
 import           Filesystem (getModified, isDirectory, isFile, canonicalizePath)
@@ -45,8 +46,7 @@ instance Read FilePath
 
 data Options = Options
     { verbose    :: Bool
-    , debug      :: Bool
-    , gitDir     :: String
+    , dryRun     :: Bool
     , remote     :: Maybe String
     , args       :: [String]
     }
@@ -54,51 +54,23 @@ data Options = Options
 options :: Parser Options
 options = Options
     <$> switch (short 'v' <> long "verbose" <> help "Display info")
-    <*> switch (short 'D' <> long "debug" <> help "Display debug")
-    <*> strOption
-        (long "git-dir" <> value ".git" <> help "Git repository to push")
+    <*> switch (short 'n' <> long "dry-run" <> help "Dry run")
     <*> argument (Just . Just) (value Nothing)
     <*> arguments Just hidden
 
 main :: IO ()
-main = withLibGitDo $ execParser opts >>= doMain
+main = withLibGitDo $ execParser opts >>= pushToGitHub
   where
     opts = info (helper <*> options)
                 (fullDesc <> progDesc desc <> header hdr)
     hdr  = "git-gpush 1.0.0 - push to GitHub with smarter behavior"
     desc = "\nDescription goes here."
 
-doMain :: Options -> IO ()
-doMain opts = do
-    -- Setup logging service if --verbose is used
-    when (verbose opts) $ initLogging (debug opts)
+volume :: Options -> Sh a -> Sh a
+volume opts = if verbose opts then verbosely else silently
 
-    -- Ask Git for the user name and email in this repository
-    (userName,userEmail) <- shelly $ silently $
-        (,) <$> (TL.init <$> run "git" ["config", "user.name"])
-            <*> (TL.init <$> run "git" ["config", "user.email"])
-
-    let gDir = fromText (TL.pack (gitDir opts))
-    isDir <- isDirectory gDir
-    gd    <- if isDir
-             then return gDir
-             else shelly $ silently $
-                  fromText . TL.init <$> run "git" ["rev-parse", "--git-dir"]
-
-    pushToGitHub opts gd
-  where
-    initLogging debugMode = do
-        let level | debugMode    = DEBUG
-                  | verbose opts = INFO
-                  | otherwise    = NOTICE
-        h <- (`setFormatter` tfLogFormatter "%H:%M:%S" "$time - [$prio] $msg")
-             <$> streamHandler System.IO.stderr level
-        removeAllHandlers
-        updateGlobalLogger "git-monitor" (setLevel level)
-        updateGlobalLogger "git-monitor" (addHandler h)
-
-pushToGitHub :: Options -> FilePath -> IO ()
-pushToGitHub opts gd = do
+pushToGitHub :: Options -> IO ()
+pushToGitHub opts = do
     -- Determine the current HEAD at the remote
     remoteName <- case remote opts of
         Nothing -> return "origin" -- jww (2013-04-28): bug
@@ -106,45 +78,77 @@ pushToGitHub opts gd = do
 
     remoteHead <-
         head . TL.words . TL.init
-            <$> (shellyNoDir $ verbosely $
+            <$> (shelly $ volume opts $
                  git [ "ls-remote", remoteName, "HEAD" ])
 
-    putStrLn $ "remoteHead is " ++ show remoteHead
+    gd <- shelly $ volume opts $
+          fromText . TL.init <$> run "git" ["rev-parse", "--git-dir"]
 
-    withRepository lgFactory gd $ do
+    void $ withRepository lgFactory gd $ do
         cref <- resolveRef "HEAD"
         for cref $ \cref -> do
             hc <- resolveCommitRef cref
             let hcoid = renderObjOid (commitOid hc)
             rcoid <- Tagged <$> parseOid (TL.toStrict remoteHead)
-            liftIO $ putStrLn $ "localHead is " ++ show hcoid
             objs <- missingObjects
                         (Just (CommitObjectId rcoid))
                         (CommitObjectId (commitOid hc))
             for_ objs $ \obj -> case obj of
                 Git.CommitObj cref -> do
                     c <- resolveCommitRef cref
-                    liftIO $ putStrLn $ "Commit: " ++ show (commitOid c)
-                    --commitLog c
+                    shelly $ volume opts $
+                        processCommitTags (commitLog c)
                 _ -> return ()
 
-    -- shellyNoDir $ verbosely $
-    --     git_ $ [ "push", remoteName ] <> map TL.pack (args opts)
-    return ()
+    unless (dryRun opts) $
+        shelly $ volume opts $
+            git_ $ [ "push", remoteName ] <> map TL.pack (args opts)
+
+data CommitTag
+    = ConfirmTag
+      { confirmIssue  :: Text
+      , confirmReport :: Maybe Text
+      }
+    deriving (Eq, Show)
+
+findTags :: Text -> [CommitTag]
+findTags = concat . map go . tails . TL.words
+  where
+    go [] = []
+    go [x] = []
+    go ["confirm", TL.splitOn ":" -> [issue]] =
+        [ ConfirmTag (TL.tail issue) Nothing | isIssue issue ]
+    go ["confirm", TL.splitOn ":" -> [issue, report]] =
+        [ ConfirmTag (TL.tail issue) (Just report) | isIssue issue ]
+    go _ = []
+
+    isIssue issue = not (TL.null issue) && TL.head issue == '#'
+
+processCommitTags :: T.Text -> Sh ()
+processCommitTags msg =
+    for_ (findTags (TL.fromStrict msg)) $ \tag -> case tag of
+        ConfirmTag issue mreport -> do
+            mreport' <- case mreport of
+                x@(Just report) -> return x
+                Nothing -> errExit False $ do
+                    who <- git [ "config", "fpco.directreport" ]
+                    ec  <- lastExitCode
+                    return $ if ec == 0
+                        then Just (TL.init who)
+                        else Nothing
+            run_ "ghi"
+                $ [ "edit", "-m"
+                  , "Please confirm that this has been fixed."
+                  , "-L", "need confirmation"
+                  ]
+               <> [ x | x <- [ "-u", fromJust mreport' ], isJust mreport' ]
+               <> [ issue ]
+
 
 git :: [Text] -> Sh Text
 git = run "git"
 
 git_ :: [Text] -> Sh ()
 git_ = run_ "git"
-
-fileStr :: FilePath -> String
-fileStr = TL.unpack . toTextIgnore
-
-infoL :: (Repository m, MonadIO m) => String -> m ()
-infoL = liftIO . infoM "git-monitor"
-
-debugL :: (Repository m, MonadIO m) => String -> m ()
-debugL = liftIO . debugM "git-monitor"
 
 -- Main.hs (git-monitor) ends here
