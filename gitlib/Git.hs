d0cb4bd324b499b8e1662ed6f3d68bec2f0ecc50
John Wiegley <johnw@fpcomplete.com>
2013-05-09 18:21:54 -0500

    Restored the Unchanged state

--- gitlib/Git.hs
+++ gitlib/Git.hs
@@ -441,7 +441,7 @@ tegRefOid (Known x) = tagOid x
 
 {- $merges -}
 
-data ModificationKind = Modified | Added | Deleted
+data ModificationKind = Unchanged | Modified | Added | Deleted
                       deriving (Eq, Ord, Enum, Show)
 
 data MergeResult m
