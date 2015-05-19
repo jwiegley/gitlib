e25c6c094f3c3b853a3689b66abc18024d870736
Michael Alan Dorman <mdorman@ironicdesign.com>
2015-05-19 07:33:15 -0400

    hgitlib2 nix updates.

--- hlibgit2/default.nix
+++ hlibgit2/default.nix
@@ -1,17 +1,14 @@
-{ cabal, bindingsDSL, zlib }:
-
-cabal.mkDerivation (self: {
+{ mkDerivation, base, bindings-DSL, git, openssl, process, stdenv
+, zlib
+}:
+mkDerivation {
   pname = "hlibgit2";
-  version = "0.18.0.13";
+  version = "0.18.0.14";
   src = ./.;
-  buildDepends = [ bindingsDSL zlib ];
-  extraLibraries = [ ];
-  # The test in hlibgit2 is extremely minimal, and rather needlessly requires
-  # git to run.  There is little point in requiring it here.
-  doCheck = false;
-  meta = {
-    description = "Low-level bindings to libgit2";
-    license = self.stdenv.lib.licenses.mit;
-    platforms = self.ghc.meta.platforms;
-  };
-})
+  buildDepends = [ base bindings-DSL zlib ];
+  testDepends = [ base process ];
+  buildTools = [ git ];
+  extraLibraries = [ openssl ];
+  description = "Low-level bindings to libgit2";
+  license = stdenv.lib.licenses.mit;
+}
