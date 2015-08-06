bd26e038aca158e19af32340ec8a58bc7b13dda3
Michael Alan Dorman <mdorman@ironicdesign.com>
2015-08-06 06:03:26 -0400

    Make git a test dependency of hlibgit2.
    This is based on the change in jgm/zip-archive#22 which was solving a
    similar issue.
    
    If you want, I'm happy to apply this change to gitlib-cmdline as well.

--- hlibgit2/Setup.hs
+++ hlibgit2/Setup.hs
@@ -1,2 +1,6 @@
-import Distribution.Simple
-main = defaultMain
+import           Distribution.Simple
+import           Distribution.Simple.Program
+
+main :: IO ()
+main = defaultMainWithHooks simpleUserHooks
+       { hookedPrograms = [ simpleProgram "git" ]}
