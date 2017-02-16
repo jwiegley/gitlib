cf7c162442abef4d48f3f7034d56da7fc2e9a4ce
John Wiegley <johnw@newartisans.com>
2017-02-15 22:13:36 -0800

    Delete many files that are no longer being maintained

--- gitlib-github/shell.nix
+++
@@ -1,9 +0,0 @@
-with (import <nixpkgs> {}).pkgs;
-
-let devEnv = haskellPackages.override {
-  overrides = self: super: {
-    gitlib = self.callPackage ../gitlib {};
-    gitlib-github = self.callPackage ./. {};
-  };
- };
-in devEnv.gitlib-github.env
--- gitlib-sample/shell.nix
+++
@@ -1,9 +0,0 @@
-with (import <nixpkgs> {}).pkgs;
-
-let devEnv = haskellPackages.override {
-  overrides = self: super: {
-    gitlib = self.callPackage ../gitlib {};
-    gitlib-sample = self.callPackage ./. {};
-  };
- };
-in devEnv.gitlib-sample.env
