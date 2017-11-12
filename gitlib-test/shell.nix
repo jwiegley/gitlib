f249b7b976d20c1060437ec3487f587d0b0c4356
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-11-12 12:01:05 +0800

    Remove all .nix files

--- gitlib-test/shell.nix
+++
@@ -1,9 +0,0 @@
-with (import <nixpkgs> {}).pkgs;
-
-let devEnv = haskellPackages.override {
-  overrides = self: super: {
-    gitlib = self.callPackage ../gitlib {};
-    gitlib-test = self.callPackage ./. {};
-  };
- };
-in devEnv.gitlib-test.env
