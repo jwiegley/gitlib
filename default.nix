7d0edc372e839c2716bea1311b61b09ce783f801
John Wiegley <johnw@newartisans.com>
2018-08-01 09:47:04 -0400

    Update default.nix

--- default.nix
+++ default.nix
@@ -2,8 +2,8 @@
 , doBenchmark ? false
 , doTracing   ? false
 , doStrict    ? false
-, rev         ? "95b1827682dc30ff1ccffb4f46c197289cea3e1c"
-, sha256      ? "0v5s2918a04h6h1m18pzp36l5f41rhkipwqgysamsz7h0q4zwhwz"
+, rev         ? "d1ae60cbad7a49874310de91cd17708b042400c8"
+, sha256      ? "0a1w4702jlycg2ab87m7n8frjjngf0cis40lyxm3vdwn7p4fxikz"
 , pkgs        ? import (builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
     inherit sha256; }) {
